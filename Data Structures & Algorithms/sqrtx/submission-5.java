640e37476a30e62b60b6c61c5c602c60a372faa8
Deepak kasturi <109023235+KasturiDeepak123@users.noreply.github.com>
2026-09-05 22:55:30 +0530

    Add: sqrtx - submission-5

---
+++ Data Structures & Algorithms/sqrtx/submission-5.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mySqrt(int x) {
+        int start = 1;
+        int end = x;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if ((long) mid * mid == x) {
+                return mid;
+            }
+            else if ((long) mid * mid < x) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+
+        return end;
+    }
+}
